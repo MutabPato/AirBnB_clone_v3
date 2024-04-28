c1cfdaf6bb7dde1409f6c348d52a2a4f3d1520d4
MutabPato <patrickmtbz@gmail.com>
2024-04-28 02:27:40 -0700

    State

---
+++ api/v1/views/states.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python3
+"""
+Create a new view for State objects
+that handles all default RESTFul API actions
+"""
+
+
+from flask import Flask, jsonify, request, abort
+from models.state import State
+from api.v1.views import app_views
+from models import storage
+
+
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def get_states():
+    states = storage.all(State).values()
+    return jsonify([state.to_dict() for state in states])
+
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def get_state(state_id):
+    """Retrieves a State object"""
+    state = storage.get(State, state_id)
+
+    if not state:
+        abort(404)
+
+    return jsonify(state.to_dict())
+
+
+@app_views.route(
+        '/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+def delete_state_id(state_id):
+    """Deletes a State object"""
+    state = storage.get(State, state_id)
+
+    if not state:
+        abort(404)
+
+    storage.delete(state)
+    storage.save()
+
+    return jsonify({}), 200
+
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def create_state():
+    """Create a new State"""
+    if not request.json:
+        abort(400, "Not a JSON")
+
+    if 'name' not in request.json:
+        abort(400, "Missing name")
+
+    data = request.json
+    state = State(**data)
+    storage.new(state)
+    storage.save()
+
+    return jsonify(state.to_dict()), 201
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def update_state(state_id):
+    """Updates a State object"""
+    state = storage.get(State, state_id)
+
+    if not state:
+        abort(404)
+
+    if not request.json:
+        abort(400, "Not a JSON")
+
+    data = request.json
+
+    ignore_keys = ['id', 'created_at', 'updated_at']
+
+    for key, value in data.items():
+        if key not in ignore_keys:
+            setattr(state, key, value)
+
+    storage.save()
+
+    return jsonify(state.to_dict()), 200
+
+
+if __name__ == '__main__':
+    host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.environ.get('HBNB_API_PORT', 5000))
+    app.run(host=host, port=port, threaded=True)
